5bdb25f835edc0e4680ec9b2bcb5502330de19f2
9112Michael <sitkabikemike@yahoo.com>
2020-09-09 10:49:25 -0700

    Rewrite  with ES6 syntax and  improve performance

--- sw.js
+++ sw.js
@@ -1,55 +1,75 @@
-var CACHE_NAME = 'my-site-cache-v1';
-let urlsToCache = [
+// Service worker design inspired by https://github.com/google-developer-training/pwa-training-labs
+
+console.log('Hello from sw.js');
+
+const urlsToCache = [
   '/',
-  '/Tab-Gallery',
-  '/index.html',
-  '/manifest.webmanifest',
-  '/style.css',
-  '/script.js'
+  'images',
+  'index.html',
+  'style.css',
+  '404.html',
+  'offline.html'
 ];
 
+const CACHE_NAME = 'my-site-cache-v3';
+
 // Listen for install event, set callback
-self.addEventListener('install', function(event) {
+self.addEventListener('install', event => {
     // Perform install steps
     event.waitUntil(
       caches.open(CACHE_NAME)
-        .then(function(cache) {
+        .then(cache => {
           console.log('Opened cache');
           return cache.addAll(urlsToCache);
         })
     );
   });
 
-  self.addEventListener('fetch', function(event) {
+  self.addEventListener('activate', event => {
+    console.log('Activating new service worker...');
+  
+    const cacheAllowlist = [CACHE_NAME];
+  
+    event.waitUntil(
+      caches.keys().then(cacheNames => {
+        return Promise.all(
+          cacheNames.map(cacheName => {
+            if (cacheAllowlist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+    );
+  });
+
+  self.addEventListener('fetch', event => {
+    console.log('Fetch event for ', event.request.url);
     event.respondWith(
       caches.match(event.request)
-        .then(function(response) {
+        .then(response => {
           // Cache hit - return response
           if (response) {
+            console.log('Found ', event.request.url, ' in cache');
             return response;
           }
-  
-          return fetch(event.request).then(
-            function(response) {
-              // Check if we received a valid response
-              if(!response || response.status !== 200 || response.type !== 'basic') {
-                return response;
-              }
-  
-              // IMPORTANT: Clone the response. A response is a stream
-              // and because we want the browser to consume the response
-              // as well as the cache consuming the response, we need
-              // to clone it so we have two streams.
-              var responseToCache = response.clone();
-  
-              caches.open(CACHE_NAME)
-                .then(function(cache) {
-                  cache.put(event.request, responseToCache);
-                });
-  
-              return response;
+          console.log('Network request for ', event.request.url);
+          return fetch(event.request)
+
+          // Add fetched files to cache
+          .then(response => {
+            if (response.status === 404) {
+              return caches.match('404.html');
             }
-          );
+            return caches.open(CACHE_NAME).then(cache => {
+              cache.put(event.request.url, response.clone());
+              return response;
+            });
+          });
+        }).catch(error => {
+          console.log('Error, ', error);
+          // Respond with custom offline page
+          return caches.match('offline.html');
         })
       );
-  });
+    });
